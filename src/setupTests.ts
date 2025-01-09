21a85d1b33a862c5ac39d9036f5ca7deefda2d6e
Harsha C G <harsha.cg@externals.adidas.com>
2025-01-09 14:28:46 +0530

    chore: initial commit

---
+++ src/setupTests.ts
@@ -0,0 +1,24 @@
+import { afterEach } from "vitest";
+import { cleanup } from "@testing-library/svelte";
+// impwort "@testing-library/jest-dom/vitest";
+
+// runs a clean after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
+
+
+// import { config } from '@some-testing-lib'
+
+// if (!globalThis.defined) {
+//   config.plugins = [myCoolPlugin]
+//   computeHeavyThing()
+//   globalThis.defined = true
+// }
+
+// // hooks are reset before each suite
+// afterEach(() => {
+//   cleanup()
+// })
+
+// globalThis.resetBeforeEachTest = true
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+// import { defineConfig } from 'vitest/config'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+// import "vitest/config"
+
+// https://vite.dev/config/
+export default defineConfig(() => (
+  {
+    plugins: [svelte()],
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './src/setupTests.ts',
+    }
+  }
+))
